f4bdf362f8eee48a6a7642968479034d72596a24
saul <43302778+saulwolfcode@users.noreply.github.com>
2019-06-11 07:20:32 -0300

    ejercicios

--- App.js
+++ App.js
@@ -1,1 +1,35 @@
-console.log("hola");
+var nombre = "saul";
+var apellido = "alejos";
+//ANY no pone valor por defecto
+var nombre2;
+console.log(nombre2);
+//tuplas
+var nombres = ["nombre", 123, true];
+console.log(nombres);
+//ENUM
+var Volumen;
+(function (Volumen) {
+    Volumen[Volumen["min"] = 1] = "min";
+    Volumen[Volumen["medio"] = 5] = "medio";
+    Volumen[Volumen["alto"] = 10] = "alto";
+})(Volumen || (Volumen = {}));
+var numeros = Volumen.min;
+console.log(numeros);
+function llamar() {
+    console.log("mostrar");
+}
+//NEVER
+// function colgar(mensaje):never{
+// throw new Error(mensaje)
+// }
+//ASERCIONES
+var cualquerValor = "lo que sea no se el valor";
+var largoString = cualquerValor.length;
+console.log(largoString);
+//NULL UNDEFINED
+//funciones
+function nombreCompleto(nombre, apellido, edad) {
+    return nombre + "" + apellido + edad;
+}
+var nombreUsuario = nombreCompleto("Saul", "alejos", 34);
+console.log(nombreUsuario);
--- App.ts
+++ App.ts
@@ -1,1 +1,36 @@
-console.log("hola peraaaaaadddd00")
+let nombre:string="saul"
+let apellido:string="alejos"
+//ANY no pone valor por defecto
+let nombre2;
+console.log(nombre2)
+//tuplas
+let nombres:[string,number,boolean]=["nombre",123,true]
+console.log(nombres)
+//ENUM
+enum Volumen{
+    min=1,
+    medio=5,
+    alto=10
+}
+let numeros:number=Volumen.min
+console.log(numeros)
+
+function llamar():void{
+    console.log("mostrar")
+}
+//NEVER
+// function colgar(mensaje):never{
+// throw new Error(mensaje)
+// }
+//ASERCIONES
+let cualquerValor:any="lo que sea no se el valor"
+let largoString:number=(<string>cualquerValor).length
+console.log(largoString)
+//NULL UNDEFINED
+//funciones
+
+function nombreCompleto(nombre:string,apellido:string,edad:number):string{
+    return nombre+""+apellido+edad
+}
+let nombreUsuario=nombreCompleto("Saul","alejos",34)
+console.log(nombreUsuario)
