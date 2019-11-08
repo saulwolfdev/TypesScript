a5230a2c2e32c306ddca2fe41e7e5a12ad5fc4e2
saul <43302778+saulwolfcode@users.noreply.github.com>
2019-11-08 17:48:49 -0300

    type

--- src/index.ts
+++ src/index.ts
@@ -1,221 +1,74 @@
-let nombre:string="saul"
-let apellido:string="alejos"
-//ANY no pone valor por defecto
-let nombre2;
-console.log(nombre2)
-//tuplas
-let nombres:[string,number,boolean]=["nombre",123,true]
-console.log(nombres)
-//ENUM
-enum Volumen{
-    min=1,
-    medio=5,
-    alto=10
-}
-let numeros:number=Volumen.min
-console.log(numeros)
+let arreglo: number[] = [1, 2, 3, 4, 4];
+let texto:string[]=["texto","texto"]
+console.log( arreglo);
+texto.push( "texto nuevo" );
+console.log( texto );
+ 
 
-function llamar():void{
-    console.log("mostrar")
-}
-//NEVER
-// function colgar(mensaje):never{
-// throw new Error(mensaje)
-// }
-//ASERCIONES
-let cualquerValor:any="lo que sea no se el valor"
-let largoString:number=(<string>cualquerValor).length
-console.log(largoString)
-//NULL UNDEFINED
-//funciones
+let wherever: any = "hola perro";
+let largoString: number = ( <string>wherever ).length;
+console.log( largoString );
 
-function nombreCompleto(nombre:string,apellido:string,edad:number):string{
-    return nombre+""+apellido+edad
+function nombre ( nombre: string, apellido: string, capitalizado:boolean=true ): string
+{
+    if (capitalizado) {
+        return capitalizar(nombre) + " " + capitalizar(apellido);
+    } else {
+        return nombre + " " + apellido;
+     }
+   
 }
-function datos(nombre:string,apellido:string,edad?:number):string{
-    if(edad){return nombre+""+apellido}
-    else{return nombre}
+function capitalizar(palabra:string):string {
+    return palabra.charAt( 0 ).toLocaleUpperCase() + palabra.substr( 1 ).toLowerCase();
 }
-let nombreUsuario=nombreCompleto("Saul","alejos",34)
-console.log(nombreUsuario)
+let data = nombre( "saul", "alejos",false );
+console.log( data );
 
-function nombreUsuarios(nombre:string,...losdemasParametros:string[]):string{
-        return nombre+""+losdemasParametros.join("")
+function nombreCompleto(nombre:string,...masDatos:string[]):string {
+    return nombre + "" + masDatos.join( " " );
 }
-let nombreUno=["Saul","Salim","Saladin","Sharan"]
-let nombreDos=["Hasan","Haiser","Haiden","Hitler"]
-console.log(nombreUno)
-console.log(nombreDos)
+let dato: string = nombreCompleto( "saul", "saul1", "saul2", "saul3" );
+let dato1: string = nombreCompleto( "alejos", "alejos1", "alejos2", "alejos3" );
+console.log( dato + dato1 )
 
-//TIPOS es una definicion de un tipo
+function sumar(a:number,b:number):number {
+    return a + b;
+}
+console.log( sumar( 1, 2 ) );
 
-type Nombre={
-    nombre:string,
-    apellido:string,
-    edad:number,
-    carrera:string[],
-    getNombre:()=>string
+
+type Heroe = {
+    nombre: string,
+    edad: number,
+    poderes: any[],
+    superVelocidad: () => string,
+    superFuerza:()=>string
 }
 
-let persona:Nombre={
-    nombre:"Saul",
-    apellido:"Alejos",
-    edad:34,
-    carrera:["Diseñador","Programador"],
-    getNombre(){
-        return this.nombre
-    }
-}
-console.log(persona)
-//UNIon de tipos
-class Datos{
-    nombre:string="Sin nombre";
-    apellido:string="Sin nombre";
-    apodo:string="Sin nombre";
-    edad:number=0
-    constructor(nombre:string,apellido:string,apodo:string,edad:number){
-        this.nombre=nombre;
-        this.nombre=apellido;
-        this.apodo=apodo;
-        this.edad=edad
-    }
-}
-let DatoNombre:Datos = new Datos("Saul","Alejos","Wolf",34)
-console.log(DatoNombre)
-//HERENCIA-CONSTRUCTORES
-class Padre{
-    constructor( public nombre:string, private apellido:string){
-        console.log("Constructor padre")
-    }
-    protected getNombre():string{
-        console.log("Get nombre Protegido")
-        return this.nombre
-    }
-
-}
-
-let padreObj:Padre = new Padre("Saul","Alejos")
-console.log("Constructor padre=>",padreObj)
-
-class Hijo extends Padre{
-constructor(a:string,b:string) {
-    console.log("Constructor hijo llamado")
-    super(a,b);
-}
-public getNombre():string{
-    console.log("Get nombre Public")
-    return super.getNombre()
-}
-}
-
-let saulHijo:Hijo = new Hijo("Saul","Alejos")
-console.log("Constructor hijo llamado =>",saulHijo.getNombre())
-
-//GETS SETS
-class Abuelo{
-    private _nombre:string;
-
-    constructor(nombre:string){
-        this._nombre=nombre
-    }
-    get nombre():string{
-        console.log("paso por get")
-        return this._nombre
-        if(this.nombre){
-            return this.nombre
-        }else{
-            return "NO tiene  nombre"
-        }
-    }
-    set nombre(nombre:string){
-        console.log("paso por set")
-        if(nombre.length<3){
-            throw new Error("Los nombre tienen que tener mas de 3 letras")
-            return
-        }
-        this._nombre=nombre
-    }
-}
-let sobrino:Abuelo = new Abuelo("el sobrino tiene nombre Saul")
-console.log(sobrino.nombre)
-
-sobrino.nombre="Sharan"
-console.log(sobrino.nombre)
-//METODOS Y PROPIEDADES ESTATICAS
-
-class Xmen{
-    static nombre:string="Wolverine"
-    constructor(){}
-    static crearXmen(){
-        console.log("se creo de forma estatica")
-        return new Xmen()
-    }
-}
-let Wolverine2=Xmen.crearXmen()
-console.log(Xmen.nombre)
-console.log(Wolverine2)
-//CLASES ABSTRACTAS
-abstract class Mutantes{
-    constructor( public nombre:string, private apellido:string){
-
-    }
-}
-class Xmen2 extends Mutantes{
-
-}
-let wolverine=new Xmen2("Saul","Alejos")
-console.log(wolverine)
-//CONSTRUCTORES PRIVADOS
-class Apocalipsis{
-    static instancia:Apocalipsis
-    private constructor(public nombre:string){
-
-    }
-    static llamarApocalipsis(){
-        if(!Apocalipsis.instancia){
-            Apocalipsis.instancia=new Apocalipsis("SOY APOCALIPSIS")
-        }
-        return Apocalipsis.instancia
-    }
-}
-let apocalips = Apocalipsis.llamarApocalipsis()
-console.log(apocalips)
-//INTERFACES
-
-interface NombreMutantes{
-    nombre:string,
-    poder?:string,
-    regenerar(nombreReal:string):void
-}
-function enviarMision(nombres:NombreMutantes){
-console.log("Enviado a "+nombres.nombre)
-    nombres.regenerar("LOGAN")
-}
-function enviarCuartel(nombres:NombreMutantes){
-    console.log("Enviado a "+nombres.nombre)
-    }
-let ciclope:NombreMutantes={
-    nombre:"xxx",
-    regenerar(x:string){
-        console.log("se regenero",x)
+let flash: Heroe = {
+    nombre: "nombre",
+    edad: 1000,
+    poderes: ["fuerza", "invisible", "rayos", "magnetismo"],
+    superFuerza ()
+    { 
+        return this.poderes[0];
     },
-    poder:"ddddd"
+    superVelocidad ()
+    {
+        return this.poderes[1];
+    }
 }
-enviarCuartel(ciclope)
-enviarMision(ciclope)
-//CLASES Y FUNCIONES INTERFASES
-
-interface XmenNuevos{
-    nombrex:string;
-    nombreReal?:string;
-    regenar(nombreReal:string):void
-    
+let superman: Heroe = {
+    nombre: "nombre dos",
+    edad: 2000,
+    poderes: ["fuerza dos", "invisible dos", "rayos dos ", "magnetismo dos"],
+    superFuerza ()
+    {
+        return this.poderes[0];
+    },
+    superVelocidad ()
+    {
+        return this.poderes[1];
+    }
 }
-// class MutantesN implements XmenNuevos{
-//     nombrex:string;
-//     regenar(nombrex){
-//             console.log(nombrex)
-//     }
-
-// }
-// let xxxx=new MutantesN()
+console.log(flash,superman) 
