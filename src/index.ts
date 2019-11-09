21107a5e85e194324fe2c6905410549bd5baee89
saul <43302778+saulwolfcode@users.noreply.github.com>
2019-11-08 23:46:47 -0300

    herencia de metodos, propìedades de padre a hijo

--- src/index.ts
+++ src/index.ts
@@ -72,3 +72,49 @@ let superman: Heroe = {
     }
 }
 console.log(flash,superman) 
+
+
+// let sama = {
+//     nombre: "Wolf",
+//     darOrden:function(){setTimeout(()=>console.log(this.nombre + " ORDEN"),1000);}
+// }
+// sama.darOrden();
+
+
+class Wolfpack { 
+    constructor ( public nombre: string, private apellido: string){ 
+        console.log("Padre llamado")
+    }
+    public getSpeed (): string
+    { 
+        console.log( "metodo publico" )
+        return this.nombre;
+       
+    }
+     protected getSpeedTwo (): string
+     {
+         console.log( "metodo protegido" )
+         return this.apellido;
+         
+    }
+}
+class WolfRed extends Wolfpack{ 
+    constructor(nombre:string,apellido:string){ 
+        super( nombre, apellido );
+        console.log( "Hijo herenda propiedades y metodos del padre" )
+    }
+    public getSpeedTwo (): string
+    {
+        console.log( "metodo protegido del padre" )
+        return super.getSpeedTwo();
+       
+    }
+}
+
+
+
+let wolf: Wolfpack = new Wolfpack( "wolf black", "wolf white" );
+let wolfTwo: WolfRed = new WolfRed( "wolf black", "wolf white" );
+console.log("soy tu padre", wolf );
+console.log("propiedades hererades del padre ", wolfTwo );
+console.log("metodo heredado del padre",wolfTwo.getSpeed());
